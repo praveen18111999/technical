05531f15e1d8b48a25fc2742e1dcad0803912f66
praveen18111999 <124046860+praveen18111999@users.noreply.github.com>
2023-04-20 11:35:21 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,23 +1,29 @@
-public class BubbleSort {
-    public static void main(String[] args) {
-        int[] arr = {5, 1, 4, 2, 8};
-        int n = arr.length;
+package letcode;
 
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
+public class bubble {
+	
+	    public static void main(String[] args) {
+	        int[] arr = {5, 1, 4, 2, 8};
+	       int temp=0;
+	      
+
+	        for (int i = 1; i <=arr.length ; i++) {
+	            for (int j = 0; j <arr.length-1; j++) {
+	                if (arr[j] > arr[j+1]) {
+	                    
+	                     temp = arr[j];
+	                    arr[j] = arr[j+1];
+	                    arr[j+1] = temp;
+	                }
+	            }
+	        }
 
 
-        System.out.println("Sorted Array:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
+	        System.out.println("Sorted Array:");
+	        for (int i = 0; i < arr.length; i++) {
+	            System.out.print(arr[i] + " ");
+	        }
+	    }
+	}
+
+
